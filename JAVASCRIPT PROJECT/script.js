c30b2961102bd43be4f5d9ee1ef441639d683a0a
Gupta7nikhil <Gup7nik@gmail.com>
2023-02-07 23:06:45 +0530

    "first commit"

---
+++ JAVASCRIPT PROJECT/script.js
@@ -0,0 +1,37 @@
+function playSound1() {
+    var sound = document.getElementById("audio1");
+    sound.play();
+}
+function playSound2() {
+    var sound = document.getElementById("audio2");
+    sound.play();
+}
+function playSound3() {
+    var sound = document.getElementById("audio3");
+    sound.play();
+}
+function playSound4() {
+    var sound = document.getElementById("audio4");
+    sound.play();
+}
+function playSound5() {
+    var sound = document.getElementById("audio5");
+    sound.play();
+}
+function playSound6() {
+    var sound = document.getElementById("audio6");
+    sound.play();
+}
+function playSound7() {
+    var sound = document.getElementById("audio7");
+    sound.play();
+}
+function playSound8() {
+    var sound = document.getElementById("audio8");
+    sound.play();
+}
+function playSound9() {
+    var sound = document.getElementById("audio9");
+    sound.play();
+}
+
